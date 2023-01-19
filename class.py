a2d1b14a8ef0eb09de5fba513d807617c2dcd6d6
Dooha005 <MINGW64@gmail.com~>
2023-01-19 11:58:19 +0300

    SOLVED LAB.

--- class.py
+++ class.py
@@ -8,13 +8,13 @@ class Panda :
         self.gender = gender
         self.age = age
 
-    def introduce_the_panda(self):
+    def introduce_panda(self):
         return f"The panda color : {self.color} , his {self.height} , gender : {self.gender} and the age : {self.age}"
 
 Describe1 = Panda("White" , 160 , "Man" , "1 year")
 Describe2 = Panda("White and balck" , 50 , "Man" , "1 mounth") 
 
 
-print(Describe1.introduce_the_panda())
+print(Describe1.introduce_panda())
 
-print(Describe2.introduce_the_panda())
+print(Describe2.introduce_panda())
