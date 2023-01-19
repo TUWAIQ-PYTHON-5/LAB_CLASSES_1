2ce42560cd0008649503d59e773a34313ab0fa9c
Sara Alotaibi <alotaibisara49@gmail.com>
2023-01-19 12:21:40 +0300

    done

--- classlab.py
+++ classlab.py
@@ -1,11 +1,11 @@
 
-#to define a class
+
 class Panda:
-    #class Attribute / Property
+    
     kind = "Animals"
 
     def __init__(self, place : str, age : int, gender : str,food :str):
-        #initialize object / instance attributes
+        
         self.place = place
         self.age = age
         self.gender = gender
@@ -17,9 +17,9 @@ class Panda:
 
 
 
-#to create a new object/instance from the class Person
+
 Panda1 = Panda("Australia",2, "male ", "pamboo")
 
 
-#calling a method
+
 print(Panda1 . hestory_about_panda())
